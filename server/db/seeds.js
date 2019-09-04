8a9c3ea5b156fcda7f90210e7d4dc5fe96359fdd
Kyle <kirvine87@gmail.com>
2019-09-04 13:50:45 +0100

    adds seeds

---
+++ server/db/seeds.js
@@ -0,0 +1,20 @@
+use bookings;
+db.dropDatabase();
+
+db..insertMany([
+  {
+    name: "Silly Billy",
+    email: "SillyBoi@gmail.com",
+    checkedIn: false
+  },
+  {
+    name: "Stevey Wevey",
+    email: "Wevey123@gmail.com",
+    checkedIn: false
+  },
+  {
+    name: "Dancy Boi",
+    email: "S1rdanc3Alot@gmail.com",
+    checkedIn: true
+  }
+]);
